db2f79d1f9a87336a54ebe28541e33065329bed2
yoren <yoren@users.noreply.github.com>
2025-08-22 16:23:39 +0000

    Deploy preview for PR 33 🛫

--- pr-preview/pr-33/assets/js/wp-a11y-docs.js
+++ pr-preview/pr-33/assets/js/wp-a11y-docs.js
@@ -58,7 +58,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '/assets/js/search-data.json', true);
+  request.open('GET', '/pr-preview/pr-33/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
