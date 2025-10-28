07a82b271f25491316539a63c0d70661c2f3b2c6
Joe Dolson <design@joedolson.com>
2025-10-28 13:34:16 -0500

    Add section to search page results

--- assets/js/wp-a11y-docs.js
+++ assets/js/wp-a11y-docs.js
@@ -617,9 +617,16 @@ jtd.onReady(function(){
         <ul>
           ${results.map(r => {
             const doc = docs[r.ref];
+            let docSection = doc.doc;
+            if ( docSection !== doc.title ) {
+              docSection = `<strong>Section:</strong> <span class="search-doc-section">${doc.doc}</span><br>`;
+            } else {
+              docSection = '';
+            }
             return `
               <li>
                 <a href="${doc.url}">${doc.title}</a><br>
+                ${docSection}
                 <small>${doc.relUrl}</small>
               </li>`;
           }).join("")}
