d9a1f3a20cb9cadcdf0fac57fb04280d5605885a
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-11-09 08:54:03 +0600

    remove copy button functionality from code blocks

--- assets/js/wp-a11y-docs.js
+++ assets/js/wp-a11y-docs.js
@@ -517,46 +517,4 @@ jtd.onReady(function(){
   initSearch();
 });
 
-// Copy button and tabindex on code blocks
-jtd.onReady(function(){
-
-  if (!window.isSecureContext) {
-    console.log('Window does not have a secure context, therefore code clipboard copy functionality will not be available. For more details see https://web.dev/async-clipboard/#security-and-permissions');
-    return;
-  }
-
-  var codeBlocks = document.querySelectorAll('div.highlighter-rouge, div.listingblock > div.content, figure.highlight');
-  var svgCopied =  '<svg viewBox="0 0 24 24" class="copy-icon"><use xlink:href="#svg-copied"></use></svg>';
-  var svgCopy =  '<svg viewBox="0 0 24 24" class="copy-icon"><use xlink:href="#svg-copy"></use></svg>';
-
-  codeBlocks.forEach(codeBlock => {
-    var copyButton = document.createElement('button');
-    var timeout = null;
-    copyButton.type = 'button';
-    copyButton.ariaLabel = 'Copy code to clipboard';
-    copyButton.ariaLive = 'polite';
-    copyButton.innerHTML = svgCopy;
-    codeBlock.append(copyButton);
-    codeBlock.tabIndex = 0;
-
-    copyButton.addEventListener('click', function () {
-      if(timeout === null) {
-        var code = (codeBlock.querySelector('pre:not(.lineno, .highlight)') || codeBlock.querySelector('code')).innerText;
-        window.navigator.clipboard.writeText(code);
-
-        copyButton.innerHTML = svgCopied;
-        copyButton.ariaLabel = 'Code is copied to clipboard';
-
-        var timeoutSetting = 4000;
-
-        timeout = setTimeout(function () {
-          copyButton.innerHTML = svgCopy;
-          timeout = null;
-        }, timeoutSetting);
-      }
-    });
-  });
-
-});
-
 })(window.jtd = window.jtd || {});
